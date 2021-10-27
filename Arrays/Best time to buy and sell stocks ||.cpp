44e70d9416ac57b83d65e5736e1fe529fcbf6646
nitishkalra <nitishkalra1998@gmail.com>
2021-10-27 23:28:44 +0530

    Best Time to buy and sell stocks ||

---
+++ Arrays/Best time to buy and sell stocks ||.cpp
@@ -0,0 +1,21 @@
+// Leetcode - best time to buy and sell stocks
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+// Find and return the maximum profit you can achieve.
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if( prices.size() == 0) return 0;
+        
+        int profit = 0;
+        for(int i=0;i<prices.size()-1;i++){
+            if(prices[i]<prices[i+1]){
+                profit += prices[i+1]-prices[i];
+            }
+        }
+        return profit;
+    }
+};
