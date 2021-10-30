596ea78d80b431748bf8064fadcc40692d17d1ed
nitishkalra <nitishkalra1998@gmail.com>
2021-10-30 12:51:37 +0530

    Remove Nth Node from list end

--- Linked Lists/Remove Nth Node From List End.c
+++ Linked Lists/Remove Nth Node From List End.c
@@ -53,3 +53,44 @@ listnode* removeNthFromEnd(listnode* head, int k) {
     return head;
     
 }
+
+// Leetcode - Remove Nth Node From End of List
+//Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head == NULL) return NULL; 
+        ListNode* temp = head;
+        int count = 0;
+        while(temp!=NULL){
+            temp = temp->next;
+            count++;
+        }
+        count = count-n;
+        temp = head;
+        if(count == 0){
+            head = temp->next;
+            return head;
+        }
+        while(count-1>0){
+            temp = temp->next;
+            count--;
+        }
+        if(temp->next){
+            temp->next = temp->next->next;
+        }
+        
+        return head;
+    }
+};
